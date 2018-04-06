a1caf45646f442cc1b07d35e99c4890e8896e885
Arno Puder <arno@puder.org>
2018-04-06 13:27:03 -0700

    Use memory-mapped I/O for faster access

--- src/esp/main/main.c
+++ src/esp/main/main.c
@@ -4,67 +4,16 @@
 static uint8_t data = 0;
 static uint8_t switched_to_out = 0;
 
-#define gpio_output_disable(gpio_num) \
-        GPIO.enable_w1tc = (0x1 << gpio_num); \
-        REG_WRITE(GPIO_FUNC0_OUT_SEL_CFG_REG + (gpio_num * 4), SIG_GPIO_OUT_IDX);
+#define GPIO_OUTPUT_DISABLE(gpio_num) GPIO.enable_w1tc = 1 << (gpio_num)
 
-#define gpio_output_enable(gpio_num) \
-        GPIO.enable_w1ts = (0x1 << gpio_num); \
-        gpio_matrix_out(gpio_num, SIG_GPIO_OUT_IDX, false, false);
+#define GPIO_OUTPUT_ENABLE(gpio_num) GPIO.enable_w1ts = 1 << (gpio_num)
 
-static void IRAM_ATTR isr_esp_sel_handler(void* arg)
-{
-    while (gpio_get_level(GPIO_NUM_23) == 1) ;
-    if (gpio_get_level(GPIO_NUM_36) == 1) {
-        // Read data
-        data = GPIO.in >> 12;
-    } else {
-        gpio_output_enable(GPIO_NUM_12);
-        gpio_output_enable(GPIO_NUM_13);
-        gpio_output_enable(GPIO_NUM_14);
-        gpio_output_enable(GPIO_NUM_15);
-        gpio_output_enable(GPIO_NUM_16);
-        gpio_output_enable(GPIO_NUM_17);
-        gpio_output_enable(GPIO_NUM_18);
-        gpio_output_enable(GPIO_NUM_19);
-        switched_to_out = 1;
-        // Write to bus
-        unsigned int d = data << 12;
-        REG_WRITE(GPIO_OUT_W1TS_REG, d);
-        d = d ^ 0b11111111000000000000;
-        REG_WRITE(GPIO_OUT_W1TC_REG, d);
-        data++;
-    }
-
-    // Release ESP_WAIT_N
-    gpio_set_level(GPIO_NUM_27, 1);
-
-    // Wait for ESP_SEL_N to be de-asserted
-    while (gpio_get_level(GPIO_NUM_23) == 1) ;
-
-    // Set SEL_WAIT_N to 0 for next IO command
-    gpio_set_level(GPIO_NUM_27, 0);
-
-    if (switched_to_out) {
-        gpio_output_disable(GPIO_NUM_12);
-        gpio_output_disable(GPIO_NUM_13);
-        gpio_output_disable(GPIO_NUM_14);
-        gpio_output_disable(GPIO_NUM_15);
-        gpio_output_disable(GPIO_NUM_16);
-        gpio_output_disable(GPIO_NUM_17);
-        gpio_output_disable(GPIO_NUM_18);
-        gpio_output_disable(GPIO_NUM_19);
-        switched_to_out = 0;
-    }
-}
 
 
 static void gpio_setup()
 {
     gpio_config_t gpioConfig;
 
-    gpio_install_isr_service(0);
-    
     // GPIO pins 12-19 (8 pins) are used for data bus
     gpioConfig.pin_bit_mask = GPIO_SEL_12 | GPIO_SEL_13 | GPIO_SEL_14 | GPIO_SEL_15 | GPIO_SEL_16 |
                               GPIO_SEL_17 | GPIO_SEL_18 | GPIO_SEL_19;
@@ -80,9 +29,7 @@ static void gpio_setup()
     
     // Configure ESP_SEL_N
     gpioConfig.pin_bit_mask = GPIO_SEL_23;
-    //gpioConfig.intr_type = GPIO_PIN_INTR_NEGEDGE;
     gpio_config(&gpioConfig);
-    gpio_isr_handler_add(GPIO_NUM_23, isr_esp_sel_handler, (void*) 0);
     
     // Configure IOBUSINT_N & ESP_WAIT_N
     gpioConfig.pin_bit_mask = GPIO_SEL_25 | GPIO_SEL_27;
@@ -103,12 +50,62 @@ static void gpio_setup()
     gpio_config(&gpioConfig);
 }
 
+
+
+static void io_cycle()
+{
+    while (GPIO.in & (1 << GPIO_NUM_23)) ;
+
+    if (GPIO.in1.data & (1 << (GPIO_NUM_36 - 32))) {
+        // Read data
+        data = GPIO.in >> 12;
+    } else {
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_12);
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_13);
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_14);
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_15);
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_16);
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_17);
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_18);
+        GPIO_OUTPUT_ENABLE(GPIO_NUM_19);
+        switched_to_out = 1;
+        // Write to bus
+        uint32_t d = data << 12;
+        REG_WRITE(GPIO_OUT_W1TS_REG, d);
+        d = d ^ 0b11111111000000000000;
+        REG_WRITE(GPIO_OUT_W1TC_REG, d);
+        data++;
+    }
+
+    // Release ESP_WAIT_N
+    GPIO.out_w1ts = (1 << GPIO_NUM_27);
+
+    // Wait for ESP_SEL_N to be de-asserted
+    while (!(GPIO.in & (1 << GPIO_NUM_23))) ;
+
+    // Set SEL_WAIT_N to 0 for next IO command
+    GPIO.out_w1tc = (1 << GPIO_NUM_27);
+
+    if (switched_to_out) {
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_12);
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_13);
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_14);
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_15);
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_16);
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_17);
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_18);
+        GPIO_OUTPUT_DISABLE(GPIO_NUM_19);
+        switched_to_out = 0;
+    }
+}
+
+
 void app_main(void)
 {
     gpio_setup();
     while (true) {
         //gpio_set_level(GPIO_NUM_25, gpio_get_level(GPIO_NUM_22) ? 0 : 1);
-        isr_esp_sel_handler(0);
+        io_cycle();
     }
 }
 
