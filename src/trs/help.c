f5ec1b0feb9b8b0b0c2f8e7bc68b8c28e0d2eb41
Arno Puder <arno@puder.org>
2018-07-08 15:19:56 -0700

    Fix for RSC URL

--- src/trs/help.c
+++ src/trs/help.c
@@ -7,7 +7,7 @@ void help()
 {
   init_window(&wnd, 0, 3, 0, 0);
   header(&wnd, "Help");
-  wnd_print(&wnd, false, "\nVisit https://retrostore.org/rsc/ for "
+  wnd_print(&wnd, false, "\nVisit https://retrostore.org/rsc for "
             "complete online documentation.");
   wnd_show(&wnd, false);
   get_key();
