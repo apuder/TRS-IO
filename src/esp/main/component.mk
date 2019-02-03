159d12a74e12517d49b34768ea4e0d825380c2a5
Arno Puder <arno@puder.org>
2019-02-02 19:29:57 -0800

    Support for CONFIG_ENABLE_TRSIO_OTA

--- src/esp/main/component.mk
+++ src/esp/main/component.mk
@@ -6,3 +6,6 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
+
+$(call compile_only_if,$(CONFIG_ENABLE_TRSIO_OTA),ota.o)
+$(call compile_only_if_not,$(CONFIG_ENABLE_TRSIO_OTA),ota-dummy.o)
---
+++ src/esp/main/ota-dummy.c
@@ -0,0 +1,13 @@
+#include "retrostore.h"
+
+void trigger_ota_check()
+{
+}
+
+void switch_to_factory()
+{
+}
+
+void init_ota()
+{
+}
