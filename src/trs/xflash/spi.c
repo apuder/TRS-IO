2b93c3543935164959188e3b9d80818f8f91ed00
Arno Puder <arno@puder.org>
2022-04-04 20:33:07 -0700

    Support for old sdcc ABI

--- src/trs/xflash/spi.c
+++ src/trs/xflash/spi.c
@@ -7,6 +7,8 @@ __sfr __at 0xfd SPI_DTA;
 #if 1
 void spiExch(unsigned char *bfr, int n) __naked
 {
+#if __SDCC_REVISION >= 12608
+// New ABI
 __asm
         ld      b,e
         ld      c,#0xfd
@@ -17,6 +19,23 @@ _exch:
         jr      nz,_exch
         ret
 __endasm;
+#else
+// Old ABI
+__asm
+        ld      iy,#2
+        add     iy,sp
+        ld      l,0(iy)
+        ld      h,1(iy)
+        ld      b,2(iy)
+        ld      c,#0xfd
+_exch:
+        ld      a,(hl)
+        out     (c),a
+        ini
+        jr      nz,_exch
+        ret
+__endasm;
+#endif
 }
 #else
 void spiExch(unsigned char *bfr, int n)
@@ -32,12 +51,27 @@ void spiExch(unsigned char *bfr, int n)
 #if 1
 void spiSend(unsigned char *bfr, int n) __naked
 {
+#if __SDCC_REVISION >= 12608
+// New ABI
 __asm
         ld      b,e
         ld      c,#0xfd
         otir
         ret
 __endasm;
+#else
+// Old ABI
+__asm
+        ld      iy,#2
+        add     iy,sp
+        ld      l,0(iy)
+        ld      h,1(iy)
+        ld      b,2(iy)
+        ld      c,#0xfd
+        otir
+        ret
+__endasm;
+#endif
 }
 #else
 void spiSend(unsigned char *bfr, int n)
@@ -52,6 +86,8 @@ void spiSend(unsigned char *bfr, int n)
 #if 1
 void spiRecv(unsigned char *bfr, int n) __naked
 {
+#if __SDCC_REVISION >= 12608
+// New ABI
 __asm
         ld      b,e
         ld      c,#0xfd
@@ -62,6 +98,23 @@ _recv:
         jr      nz,_recv
         ret
 __endasm;
+#else
+// Old ABI
+__asm
+        ld      iy,#2
+        add     iy,sp
+        ld      l,0(iy)
+        ld      h,1(iy)
+        ld      b,2(iy)
+        ld      c,#0xfd
+        xor     a
+_recv:
+        out     (c),a
+        ini
+        jr      nz,_recv
+        ret
+__endasm;
+#endif
 }
 #else
 void spiRecv(unsigned char *bfr, int n)
