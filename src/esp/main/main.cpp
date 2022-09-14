e1036d8b6b4eeb3214a4958d92c66ca116c4f502
Arno Puder <arno@puder.org>
2022-09-14 11:43:16 -0700

    Log TRS-IO's git commit on boot

--- src/esp/main/main.cpp
+++ src/esp/main/main.cpp
@@ -7,6 +7,7 @@
 #include "storage.h"
 #include "event.h"
 #include "esp_event.h"
+#include "esp_log.h"
 
 #include "retrostore.h"
 #include "backend.h"
@@ -15,10 +16,15 @@
 #include "spi.h"
 
 
+extern const char* GIT_REV;
+extern const char* GIT_BRANCH;
+
 extern "C" {
 
 void app_main(void)
 {
+  ESP_LOGI("main", "TRS-IO branch=%s, rev=%s", GIT_BRANCH, GIT_REV);
+
   init_events();
   init_trs_io();
   init_led();
