729c1163efda1c6981a9e0d432cd2971d74fc152
Arno Puder <arno@puder.org>
2023-02-14 11:03:00 -0800

    Fix GPIO pin assignments for TRS-IO++

--- src/esp/main/io.cpp
+++ src/esp/main/io.cpp
@@ -23,8 +23,12 @@
 #include "spi.h"
 
 #define ESP_REQ GPIO_NUM_34
+#ifdef CONFIG_TRS_IO_PP
+#define ESP_DONE GPIO_NUM_18
+#else
 #define ESP_DONE GPIO_NUM_27
 #define ESP_FULL_ADDR GPIO_NUM_33
+#endif
 
 
 #define ADJUST(x) ((x) < 32 ? (x) : (x) - 32)
@@ -33,8 +37,10 @@
 #define MASK64_ESP_REQ (1ULL << ESP_REQ)
 #define MASK_ESP_DONE (1 << ADJUST(ESP_DONE))
 #define MASK64_ESP_DONE (1ULL << ESP_DONE)
+#ifndef CONFIG_TRS_IO_PP
 #define MASK_ESP_FULL_ADDR (1 << ADJUST(ESP_FULL_ADDR))
 #define MASK64_ESP_FULL_ADDR (1ULL << ESP_FULL_ADDR)
+#endif
 
 
 static volatile bool DRAM_ATTR trigger_trs_io_action = false;
@@ -158,7 +164,11 @@ void trx_write_memory(uint16_t addr, uint8_t value) {
 
 static void init_xray()
 {
+#ifndef CONFIG_TRS_IO_PP
   bool full_addr = !gpio_get_level(ESP_FULL_ADDR);
+#else
+  bool full_addr = false;
+#endif
 
   TRX_Context* ctx = get_default_trx_context();
   ctx->system_name = "Model 1";
@@ -286,8 +296,13 @@ static void IRAM_ATTR io_task(void* p)
   while(true) {
     while (!esp_req_triggered) ;
     esp_req_triggered = false;
+#ifndef CONFIG_TRS_IO_PP
     // Read pins [S2..S0,A3..A0]
     const uint8_t s = GPIO.in >> 12;
+#else
+    // Read pins [S3..S0] to upper nibble
+    const uint8_t s = GPIO.in >> 8;
+#endif
     switch(s & 0x70) {
     case 0x00:
       trs_io_write();
@@ -408,11 +423,14 @@ void init_io()
 
   gpio_config_t gpioConfig;
 
-#ifdef CONFIG_TRS_IO_MODEL_1
+#if defined(CONFIG_TRS_IO_MODEL_1)
 // GPIO pins 12-18 (7 pins) are used for S0-S2 and A0-A4
   gpioConfig.pin_bit_mask = GPIO_SEL_12 | GPIO_SEL_13 | GPIO_SEL_14 |
     GPIO_SEL_15 | GPIO_SEL_16 |
     GPIO_SEL_17 | GPIO_SEL_18;
+#elif defined(CONFIG_TRS_IO_PP)
+  // GPIO pins 12-15 (4 pins) are used for S0-S3
+  gpioConfig.pin_bit_mask = GPIO_SEL_12 | GPIO_SEL_13 | GPIO_SEL_14 | GPIO_SEL_15;
 #else
   // GPIO pins 16-18 (3 pins) are used for S0-S2
   gpioConfig.pin_bit_mask = GPIO_SEL_16 | GPIO_SEL_17 | GPIO_SEL_18;
