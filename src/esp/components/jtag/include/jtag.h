b466775c6bce9b0564eda6dabfb73c39df9ece77
Arno Puder <arno@puder.org>
2023-09-13 15:00:27 -0700

    Add JTAG interface for the 20K

---
+++ src/esp/components/jtag/include/jtag.h
@@ -0,0 +1,246 @@
+
+#pragma once
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include <cstring>
+
+#define NOOP				0x02
+#define ERASE_SRAM			0x05
+#define READ_SRAM			0x03
+#define XFER_DONE			0x09
+#define READ_IDCODE			0x11
+#define INIT_ADDR			0x12
+#define READ_USERCODE		0x13
+#define CONFIG_ENABLE		0x15
+#define XFER_WRITE			0x17
+#define CONFIG_DISABLE		0x3A
+#define RELOAD				0x3C
+#define STATUS_REGISTER		0x41
+#  define STATUS_CRC_ERROR			(1 << 0)
+#  define STATUS_BAD_COMMAND		(1 << 1)
+#  define STATUS_ID_VERIFY_FAILED	(1 << 2)
+#  define STATUS_TIMEOUT			(1 << 3)
+#  define STATUS_MEMORY_ERASE		(1 << 5)
+#  define STATUS_PREAMBLE			(1 << 6)
+#  define STATUS_SYSTEM_EDIT_MODE	(1 << 7)
+#  define STATUS_PRG_SPIFLASH_DIRECT (1 << 8)
+#  define STATUS_NON_JTAG_CNF_ACTIVE (1 << 10)
+#  define STATUS_BYPASS				(1 << 11)
+#  define STATUS_GOWIN_VLD			(1 << 12)
+#  define STATUS_DONE_FINAL			(1 << 13)
+#  define STATUS_SECURITY_FINAL		(1 << 14)
+#  define STATUS_READY				(1 << 15)
+#  define STATUS_POR				(1 << 16)
+#  define STATUS_FLASH_LOCK			(1 << 17)
+
+#define ERR_JTAG_UNEXPECTED_IR_LENGTH -1
+#define ERR_JTAG_UNEXPECTED_DEVICES -2
+#define ERR_JTAG_UNEXPECTED_IDCODE -3
+
+#if 0
+// Brown -> GPIO 16
+#define TDO 0x08
+// Orange -> GPIO 15
+#define TDI 0x04
+// Red -> GPIO 14
+#define TMS 0x02
+// Yellow -> GPIO 13
+#define TCK 0x01
+#else
+// Brown -> GPIO 16
+#define TDO (1 << (39 - 32))
+// Orange -> GPIO 15
+#define TDI (1 << 27)
+// Red -> GPIO 14
+#define TMS (1 << 5)
+// Yellow -> GPIO 13
+#define TCK (1 << 23)
+#endif
+
+// GW2A18PG256: IR length = 8
+#define IR_SAMPLE_PRELOAD 0x01
+#define IR_READ_USERCODE 0x13
+#define IR_IDCODE 0x11
+#define IR_HIGHZ 0x0C
+#define IR_BYPASS 0xFF
+
+
+
+class BitstreamSource {
+public:
+  virtual bool open() = 0;
+  virtual bool read(void* buf, int n, int* br) = 0;
+  virtual bool close() = 0;
+};
+
+class JTAGAdapter {
+private:
+  inline void outport(uint32_t d) {
+    //d = (d & 7) << 13;
+    GPIO.out_w1ts = d & (TCK | TMS | TDI);
+    GPIO.out_w1tc = (~d) & (TCK | TMS | TDI);
+  }
+
+  inline bool inport() {
+    //return GPIO.in >> 13;
+    return GPIO.in1.data & TDO;
+  }
+
+public:
+  virtual void setup() = 0;
+  int determineChainLength();
+  int scan(int expected_ir_len, int expected_devices, uint32_t expected_idcode);
+
+  inline bool pulse(uint32_t d) {
+	  bool tdo;
+
+    outport(d);
+    tdo = inport();
+    outport(d | TCK);
+    outport(d);
+
+    return tdo;
+  }
+
+  inline void reset() {
+    for(int i = 0; i < 5; i++) {
+      pulse(TMS);
+    }
+
+    // Go to Run-Test/Idle
+	  pulse(0);
+  }
+
+  inline void toggleClk(int n) {
+    for (int i = 0; i < n; i++) {
+      pulse(0);
+    }
+  }
+
+  inline void enterShiftIR() {
+    pulse(TMS);
+    pulse(TMS);
+    pulse(0);
+    pulse(0);
+  }
+
+  inline void enterShiftDR() {
+    pulse(TMS);
+    pulse(0);
+    pulse(0);
+  }
+
+  inline void exitShift() {
+    pulse(TMS);
+    pulse(TMS);
+    pulse(0);
+  }
+
+  // note: call this function only when in shift-IR or shift-DR state
+  inline void sendDataLSB(void* d, int bitlength, bool exit) {
+    int bitofs = 0;
+    uint8_t* p = (uint8_t*) d;
+
+    bitlength--;
+    while(bitlength--) {
+      pulse((p[bitofs >> 3] >> (bitofs & 7) & 1) ? TDI : 0);  // send all bits but the last one
+      bitofs++;
+    }
+
+    uint32_t tms = exit ? TMS : 0;
+    pulse((p[bitofs >> 3] >> (bitofs & 7) & 1) ? TDI | tms : tms);  // send last bit, with TMS asserted
+  }
+
+  // note: call this function only when in shift-IR or shift-DR state
+  inline void sendDataMSB(void* d, int bitlength, bool exit) {
+    int bitofs = 0;
+    uint8_t* p = (uint8_t*) d;
+
+    bitlength--;
+    while(bitlength--) {
+      pulse((p[bitofs >> 3] << (bitofs & 7) & 0x80) ? TDI : 0);  // send all bits but the last one
+      bitofs++;
+    }
+    uint32_t tms = exit ? TMS : 0;
+    pulse((p[bitofs >> 3] << (bitofs & 7) & 0x80) ? TDI | tms : tms);  // send last bit, with TMS asserted
+  }
+
+  // note: call this function only when in shift-IR or shift-DR state
+  void readData(void* d, int bitlength) {
+    int bitofs = 0;
+    uint8_t* p = (uint8_t*) d;
+
+    memset(p, 0, (bitlength + 7) / 8);
+
+    bitlength--;
+    while(bitlength--) {
+      // Read all bits but the last one
+      p[bitofs / 8] |= (pulse(0) << (bitofs & 7));
+      bitofs++;
+    }
+
+    // Read the last bit, with TMS asserted
+    p[bitofs / 8] |= (pulse(TMS) << (bitofs & 7));
+
+    // Return to state Run-Test/Idle
+    pulse(TMS);
+    pulse(0);
+  }
+
+  // note: call this function only when in shift-IR or shift-DR state
+  void readDataMSB(void* d, int bitlength) {
+    int bitofs = 0;
+    uint8_t* p = (uint8_t*) d;
+
+    memset(p, 0, (bitlength + 7) / 8);
+
+    bitlength--;
+    while(bitlength--) {
+      // Read all bits but the last one
+      p[bitofs / 8] |= (pulse(0) << (7 - (bitofs & 7)));
+      bitofs++;
+    }
+
+    // Read the last bit, with TMS asserted
+    p[bitofs / 8] |= (pulse(TMS) << (7 - (bitofs & 7)));
+
+    // Return to state Run-Test/Idle
+    pulse(TMS);
+    pulse(0);
+  }
+
+  inline void readDR(void* p, int bitlength) {
+    enterShiftDR();
+    readData(p, bitlength);
+    toggleClk(6);
+  }
+
+  inline void setIR(uint32_t ir) {
+    enterShiftIR();
+    sendDataLSB(&ir, 8, true);
+    pulse(TMS);
+    pulse(0);  // go back to Run-Test/Idle
+    toggleClk(6);
+  }
+
+  uint32_t readStatusReg();
+  bool pollFlag(uint32_t mask, uint32_t value);
+  bool enableCfg();
+  bool disableCfg();
+  bool eraseSRAM();
+  bool programToSRAM(BitstreamSource* bs, bool log);
+
+};
+
+class JTAGAdapterTrsIO : public JTAGAdapter {
+public:
+  void setup();
+  void testBoundaryScan();
+  void testProgramToSRAM();
+  void testProgramToFLASH();
+};
+
---
+++ src/esp/components/jtag/jtag.cpp
@@ -0,0 +1,351 @@
+
+// TN653E
+// http://cdn.gowinsemi.com.cn/TN653E.pdf
+
+
+///////////////////////////////////////////////////////////
+#include "esp_log.h"
+#include <stdio.h>
+#include <assert.h>
+
+
+#include "trs-fs.h"
+#include "jtag.h"
+
+
+void JTAG_SendDataMSB(char* p, int bitlength, bool exit);
+
+
+class BitstreamSourceFile : public BitstreamSource {
+private:
+  const char* fn;
+  FIL f;
+
+public:
+  BitstreamSourceFile(const char* fn) {
+    this->fn = fn;
+  }
+
+  bool open() {
+    if (trs_fs == NULL || trs_fs->get_err_msg() != NULL) {
+      return false;
+    }
+    if (trs_fs->f_open(&f, fn, FA_READ) != FR_OK) {
+      return false;
+    }
+    return true;
+  }
+
+  bool read(void* buf, int n, int* br) {
+    UINT _br;
+
+    if (trs_fs->f_read(&f, buf, n, &_br) != FR_OK) {
+      return false;
+    }
+    *br = (int) _br;
+    return true;
+  }
+
+  bool close() {
+    if (trs_fs->f_close(&f) != FR_OK) {
+      return false;
+    }
+    return true;
+  }
+};
+
+
+int JTAGAdapter::determineChainLength()
+{
+  int i;
+
+  // Fill chain with 0s
+  for(i = 0; i < 1000; i++) {
+    pulse(0);
+  }
+
+  // Fill chain with 1s and stop when the first 1 is read at TDO
+  for(i = 0; i < 1000; i++) {
+    if (pulse(TDI)) break;
+  }
+  exitShift();
+  return i;
+}
+
+int JTAGAdapter::scan(int expected_ir_len, int expected_devices, uint32_t expected_idcode)
+{
+  reset();
+  enterShiftIR();
+  if (determineChainLength() != expected_ir_len) {
+    return ERR_JTAG_UNEXPECTED_IR_LENGTH;
+  }
+
+  // we are in BYPASS mode since JTAG_DetermineChainLength filled the IR chain full of 1's
+  // now we can easily determine the number of devices (= DR chain length when all the devices are in BYPASS mode)
+  enterShiftDR();
+  int devices = determineChainLength();
+  if (devices != expected_devices) {
+    return ERR_JTAG_UNEXPECTED_DEVICES;
+  }
+
+  // read the IDCODEs (assume all devices support IDCODE, so read 32 bits per device)
+  reset();
+  uint32_t idcode[devices];
+  readDR(idcode, 32 * devices);
+  for(int i = 0; i < devices; i++) {
+    if (idcode[i] != expected_idcode) {
+      return ERR_JTAG_UNEXPECTED_IDCODE;
+    }
+  }
+  return 0;
+}
+
+uint32_t JTAGAdapter::readStatusReg()
+{
+  uint32_t reg;
+  uint8_t rx[4];
+
+  setIR(STATUS_REGISTER);
+  readDR(rx, sizeof(rx) * 8);
+  reg = rx[3] << 24 | rx[2] << 16 | rx[1] << 8 | rx[0];
+  return reg;
+}
+
+bool JTAGAdapter::pollFlag(uint32_t mask, uint32_t value) {
+  uint32_t status;
+  long timeout = 0;
+
+  do {
+    status = readStatusReg();
+    if (timeout == 100000000){
+      return false;
+    }
+    timeout++;
+  } while ((status & mask) != value);
+
+  return true;
+}
+
+bool JTAGAdapter::enableCfg()
+{
+  setIR(CONFIG_ENABLE);
+  return pollFlag(STATUS_SYSTEM_EDIT_MODE, STATUS_SYSTEM_EDIT_MODE);
+}
+
+bool JTAGAdapter::disableCfg()
+{
+  setIR(CONFIG_DISABLE);
+  setIR(NOOP);
+  return pollFlag(STATUS_SYSTEM_EDIT_MODE, 0);
+}
+
+/* Erase SRAM:
+ * TN653 p.9-10, 14 and 31
+ */
+bool JTAGAdapter::eraseSRAM()
+{
+  setIR(ERASE_SRAM);
+  setIR(NOOP);
+
+  /* TN653 specifies to wait for 4ms with
+  * clock generated but
+  * status register bit MEMORY_ERASE goes low when ERASE_SRAM
+  * is send and goes high after erase
+  * this check seems enough
+  */
+  if (pollFlag(STATUS_MEMORY_ERASE, STATUS_MEMORY_ERASE)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool JTAGAdapter::programToSRAM(BitstreamSource* bs, bool log)
+{
+  setIR(READ_IDCODE);
+  uint32_t id;
+  readDR(&id, 32);
+  if (id != 0x081b) {
+    return false;
+  }
+
+  /* erase SRAM */
+  if (!enableCfg()) {
+    return false;
+  }
+  if (!eraseSRAM()) {
+    return false;
+  }
+  if (!disableCfg()) {
+    return false;
+  }
+
+  /* load bitstream in SRAM */
+  if (!enableCfg()) {
+    return false;
+  }
+
+  setIR(XFER_WRITE);  // Transfer Configuration Data
+  enterShiftDR();
+
+  unsigned long count = 0;
+  unsigned long total_count = 0;
+  int br;
+
+  do {
+    static char buf[1024];
+
+    if (!bs->read(buf, sizeof(buf), &br)) {
+      return false;
+    } else {
+      if (br != 0) {
+        sendDataMSB(buf, br * 8, br != sizeof(buf));
+        if (log) {
+          count += br;
+          total_count += br;
+          if (count > 100000) {
+            count -= 100000;
+            ESP_LOGI("JTAG", "Bytes written: %ld", total_count);
+          }
+        }
+      }
+    }
+  } while (br != 0);
+  pulse(TMS);
+  pulse(0);  // go back to Run-Test/Idle
+
+  setIR(XFER_DONE);  // XFER_DONE
+  if (!pollFlag(STATUS_DONE_FINAL, STATUS_DONE_FINAL)) {
+    return false;
+  }
+      
+  return disableCfg();
+}
+
+void JTAGAdapterTrsIO::setup()
+{
+  gpio_config_t gpioConfig;
+
+  gpioConfig.pin_bit_mask = GPIO_SEL_5 | GPIO_SEL_23 | GPIO_SEL_27;
+  gpioConfig.mode = GPIO_MODE_OUTPUT;
+  gpioConfig.pull_up_en = GPIO_PULLUP_DISABLE;
+  gpioConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  gpioConfig.intr_type = GPIO_INTR_DISABLE;
+  gpio_config(&gpioConfig);
+
+  gpioConfig.pin_bit_mask = GPIO_SEL_39;
+  gpioConfig.mode = GPIO_MODE_INPUT;
+  gpio_config(&gpioConfig);
+}
+
+
+uint8_t reverse(uint8_t v)
+{
+  uint8_t b = 0;
+  for(int i = 0; i < 8; i++) {
+    b <<= 1;
+    b |= (v & 1);
+    v >>= 1;
+  }
+  return b;
+}
+
+///////////////////////////////////////////////////////////
+
+/*
+ * Uses the JTAG boundary scan to toggle the pads of the 20K. This
+ * will make the four LEDs on the TRS-IO++ board blink. The blinking
+ * is not caused by the FPGA, but by the ESP toggling the pads.
+ */
+void JTAGAdapterTrsIO::testBoundaryScan()
+{
+  setup();
+  reset();
+
+  uint32_t LED = 0;
+
+  while (1) {
+    LED ^= TDI;
+    const uint8_t EXTEST = 0x04;
+
+    setIR(EXTEST);
+    enterShiftDR();
+
+    for (int i = 0; i < 760 - 1; i++) {
+      switch (i) {
+        case 337: // LED[3] == D10
+        case 339: // LED[2] == E10
+        case 687: // LED[1] == R7
+        case 685: // LED[0] == P7
+          pulse(LED);
+          break;
+        default:
+          pulse(0);
+          break;
+      }
+    }
+    pulse(TMS);
+    pulse(0);
+    pulse(TMS);
+    pulse(TMS);
+    pulse(0);
+
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+  // Never reached
+}
+
+void JTAGAdapterTrsIO::testProgramToSRAM()
+{
+  setup();
+  reset();
+  // Section 2.2.5, IDCODE for GW2A(R)-18
+  int err = scan(8, 1, 0x0000081B);
+  if (err != 0) {
+    ESP_LOGE("JTAG", "Unexpected scan result: %d", err);
+    return;
+  }
+
+  BitstreamSourceFile* bs = new BitstreamSourceFile("firmware.bin");
+  if (!bs->open()) {
+    ESP_LOGE("JTAG", "Open failed");
+  } else {
+    ESP_LOGI("JTAG", "Programming to SRAM");
+    if (!programToSRAM(bs, true)) {
+      ESP_LOGE("JTAG", "Program failed");
+      return;
+    }
+    if (!bs->close()) {
+      ESP_LOGE("JTAG", "Close failed");
+      return;
+    }
+    ESP_LOGI("JTAG", "Programming SRAM succeeded");
+  }
+}
+
+void JTAGAdapterTrsIO::testProgramToFLASH()
+{
+  setup();
+  reset();
+
+  setIR(0x16); // Sec 2.2.12 Bridge to SPI
+  pulse(TMS);  // Figure 2.22
+  pulse(0);
+  pulse(0);
+  pulse(TMS);
+  pulse(0);
+  pulse(TMS);
+  pulse(0); // Shift-DR
+  char b = 0x9f; // XTX: Sec 6.25 Read Identification (RDID)
+  sendDataMSB(&b, 8, false);
+  pulse(0);
+  char id[3];
+  readDataMSB(id, 3 * 8);
+  ESP_LOGI("JTAG", "Flash ID: %02x %02x %02x", id[0], id[1], id[2]);
+
+  // Return to Test-Logic Reset
+  for(int i = 0; i < 5; i++) {
+    pulse(TMS);
+  }
+}
+
