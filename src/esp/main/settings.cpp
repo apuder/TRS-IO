e5f5f3de92a1a67e7189cab57a964cf1230b32dc
Arno Puder <arno@puder.org>
2024-10-13 15:00:55 -0700

    Make FreHD-enabled ROMs the default for all models

--- src/esp/main/settings.cpp
+++ src/esp/main/settings.cpp
@@ -161,11 +161,11 @@ static void init_smb_credentials()
 static vector<string> roms;
 
 static const char* default_roms[] = {
-  "level2.bin",
+  "level2-frehd.bin",
   "",
-  "model3.bin",
-  "model3.bin",
-  "model3.bin"
+  "model3-frehd.bin",
+  "model3-frehd.bin",
+  "model3-frehd.bin"
 };
 
 vector<string>& settings_get_roms()
